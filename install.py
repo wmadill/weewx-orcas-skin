66ae7ca561a65c7e6b0a1774f209782c677314b8
Bill Madill <bill@jamimi.com>
2019-08-22 10:50:22 -0700

    Fix "daylight hours" rounding bug where minutes display "60"; roll version number

--- install.py
+++ install.py
@@ -8,7 +8,7 @@ def loader():
 class OrcasInstaller(ExtensionInstaller):
     def __init__(self):
         super(OrcasInstaller, self).__init__(
-            version="1.6",
+            version="1.6.1",
             name='Orcas',
             description='Skin for Orcas weather station',
             author='Bill Madill',
